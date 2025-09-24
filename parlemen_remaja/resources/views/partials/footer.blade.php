87c488ba4e41328be9d12515974803aaeb118ba3
Xerzom <155271840+Xerzom@users.noreply.github.com>
2025-09-24 17:19:47 +0700

    Delete parlemen_remaja directory

--- parlemen_remaja/resources/views/partials/footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-dark text-white py-3 mt-4">
-    <div class="container text-center">
-        <p class="mb-0">&copy; {{ date('Y') }} Parlemen Remaja. All rights reserved.</p>
-    </div>
-</footer>
